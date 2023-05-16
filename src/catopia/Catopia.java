0e4566ce1738d5ab91b1c2b62c9dbbee23e5d9d0
Ruba_Bakhour <rabdullahbakhour@stu.kau.edu.sa>
2023-05-16 23:06:43 +0300

    initial commit

---
+++ src/catopia/Catopia.java
@@ -0,0 +1,17 @@
+
+package catopia;
+
+/**
+ *
+ * @author Ruba
+ */
+public class Catopia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
